01142e35a54ec03c034dafbfa7ce0daadb112cb7
Rineta2 <rzkir571@gmail.com>
2025-05-28 02:43:27 +0700

    [update] delete animasi for dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -39,9 +39,6 @@ function DialogOverlay({
       data-slot="dialog-overlay"
       className={cn(
         "fixed inset-0 z-50 bg-black/60 backdrop-blur-sm",
-        "data-[state=open]:animate-in data-[state=closed]:animate-out",
-        "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-        "duration-300 ease-in-out",
         className
       )}
       {...props}
@@ -61,12 +58,7 @@ function DialogContent({
         data-slot="dialog-content"
         className={cn(
           "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%]",
-          "gap-4 border bg-background p-6 shadow-lg duration-200",
-          "data-[state=open]:animate-in data-[state=closed]:animate-out",
-          "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-          "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-          "data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
-          "data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+          "gap-4 border bg-background p-6 shadow-lg",
           "h-full max-h-screen sm:h-auto sm:max-h-[85vh]",
           "max-w-[100vw] sm:max-w-lg",
           "sm:rounded-lg md:w-full",
