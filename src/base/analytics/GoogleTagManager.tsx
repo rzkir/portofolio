841f139a85f5dd6d0091b2c98fa1e16d62c862ec
Rineta2 <rzkir571@gmail.com>
2025-06-14 12:09:00 +0700

    [update] google tag manager

--- src/base/analytics/GoogleTagManager.tsx
+++ src/base/analytics/GoogleTagManager.tsx
@@ -1,6 +1,6 @@
 import Script from 'next/script'
 
-const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER
+const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
 export const GoogleTagManager = () => {
     return (
